2923e8600449541290d09bfa000aa140c93f41f6
honeykokia <honeykokia@gmail.com>
2025-07-02 22:25:04 +0800

    feat:config/CorsDebugFilter - Add filter to monitor request Origin header

---
+++ src/main/java/com/example/demo/config/CorsDebugFilter.java
@@ -0,0 +1,22 @@
+package com.example.demo.config;
+
+import java.io.IOException;
+
+import org.springframework.stereotype.Component;
+
+import jakarta.servlet.Filter;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.ServletRequest;
+import jakarta.servlet.ServletResponse;
+import jakarta.servlet.http.HttpServletRequest;
+
+public class CorsDebugFilter implements Filter {
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
+            throws IOException, ServletException {
+        HttpServletRequest req = (HttpServletRequest) request;
+        System.out.println("Request Origin: " + req.getHeader("Origin"));
+        chain.doFilter(request, response);
+    }
+}
