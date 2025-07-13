7b52bd3eb310ec5596fe3eb5c7299161877f2f64
Strawberry <honeykokia@gmail.com>
2025-07-13 16:01:47 +0800

    chore: Align controller with API spec exapmle

--- src/main/java/com/example/demo/controllers/CategoryController.java
+++ src/main/java/com/example/demo/controllers/CategoryController.java
@@ -24,17 +24,7 @@ public class CategoryController {
         category1.setName("耳機");
         category1.setDescription("各式藍芽耳機");
 
-        CategoryInfo category2 = new CategoryInfo();
-        category2.setId(2L);
-        category2.setName("手機");
-        category2.setDescription("智慧型手機");
-
-        CategoryInfo category3 = new CategoryInfo();
-        category3.setId(3L);
-        category3.setName("電腦");
-        category3.setDescription("筆記型電腦與桌上型電腦");
-
-        ApiResponse response = new ApiResponse(Map.of("categories", List.of(category1, category2, category3)));
+        ApiResponse response = new ApiResponse(Map.of("categories", List.of(category1)));
         return ResponseEntity.ok(response);
     }
 
