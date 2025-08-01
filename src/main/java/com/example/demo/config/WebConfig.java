cfdbec80cdc8bf6ec0c2e27d61f19a04bc72d944
honeykokia <72547838+honeykokia@users.noreply.github.com>
2025-08-01 22:06:38 +0800

    feat: Add sub-respository, JWT authentication, password encryption, spring security, generic validator and Update register flow (#34)
    * feat: Add sub-respository `ecommerce-docs`
    
    * feat: Add JWT authentication, password encryption, and Spring Security
    - `pom.xml`: Add dependency for jwt, bcrypt and spring-security
    - `JwtUtil.java`: Define Jwt generate token format and extract the userId from token
    - `JwtFilter.java`: When a User sends request, the JwtFilter intercepts the request and parses the JWT token
    - `SecurityConfig`: Define which request need to pass though the JwtFilter and specifies the allowed request origin
    - `AppConfig`:  When a User register or login, their password is encrypted and compared
    
    * feat: Add generic validator design and implement login authentication
    - `CustValidator.kava`: Define custom validation methods
    - `LoginValidator.java`: Vaildator user login by checking email existence and matching password with database
    
    * chore: Update WebConfig and register flow
    - `LoginRequest`: Add @NotBlank and @NotNull to field
    - `UserService`: Implement register logic
    - `UserController`: Refactor register to user userservice and remove hardcode data
    - `WebConfig`: Removed @Configuration to avoid conflict with `SecurityConfig`

--- src/main/java/com/example/demo/config/WebConfig.java
+++ src/main/java/com/example/demo/config/WebConfig.java
@@ -1,11 +1,13 @@
 package com.example.demo.config;
 
+import java.util.Arrays;
+
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-@Configuration
+
 public class WebConfig implements WebMvcConfigurer {
 
     @Value("${cors.allowed-origins}")
@@ -15,8 +17,8 @@ public class WebConfig implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
                 .allowedOrigins(allowedOrigins)
-                .allowedMethods("*")
-                .allowedHeaders("*")
+                .allowedMethods("GET", "POST", "PUT", "DELETE","PATCH")
+                .allowedHeaders("Authorization", "Content-Type")
                 .allowCredentials(true);
     }
 }
--- src/main/java/com/example/demo/services/UserService.java
+++ src/main/java/com/example/demo/services/UserService.java
@@ -1,16 +1,22 @@
 package com.example.demo.services;
 
 import java.time.LocalDateTime;
+import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import com.example.demo.bean.UserBean;
+import com.example.demo.dto.LoginInfo;
+import com.example.demo.dto.LoginRequest;
 import com.example.demo.dto.RegisterRequest;
 import com.example.demo.dto.UserInfo;
 import com.example.demo.enums.UserRole;
 import com.example.demo.repository.UserRepository;
 import com.example.demo.responses.ApiResponse;
+import com.example.demo.utils.JwtUtil;
+import com.example.demo.vaildator.LoginValidator;
 
 @Service
 public class UserService {
@@ -18,12 +24,36 @@ public class UserService {
     @Autowired
     private UserRepository userRepository;
 
+    @Autowired
+    private LoginValidator loginValidator;
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+    @Autowired
+    private JwtUtil jwtUtil;
+
+    public ApiResponse login(LoginRequest loginRequest) {
+
+        UserBean user = loginValidator.validate(loginRequest);       
+        String token = jwtUtil.generateToken(user.getEmail(), user.getId());
+
+        LoginInfo loginInfo = new LoginInfo();
+        loginInfo.setName(user.getName());
+        loginInfo.setEmail(user.getEmail());
+        loginInfo.setToken(token);
+
+        return new ApiResponse(Map.of("user",loginInfo));
+    }
+
     public ApiResponse register(RegisterRequest registerRequest) {
+
+        String encodedPassword = passwordEncoder.encode(registerRequest.getPassword());
 
         UserBean user = new UserBean();
         user.setName("新加入會員");
         user.setEmail(registerRequest.getEmail());
-        user.setPassword(registerRequest.getPassword());
+        user.setPassword(encodedPassword);
         user.setImage("/upload/defaultAvatar.png");
         user.setGender((byte) 1);
         user.setBirthday(null);
@@ -41,7 +71,7 @@ public class UserService {
         userInfo.setName(user.getName());
         userInfo.setEmail(user.getEmail());
 
-        return new ApiResponse(userInfo);
+        return new ApiResponse(Map.of("user", userInfo));
 
     }
 }
