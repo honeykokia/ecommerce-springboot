ac0c7a03520428d878e82777b671e52c4d12efff
Strawberry <honeykokia@gmail.com>
2025-08-05 19:43:37 +0800

    chore(LoginRequest.java): Remove @NotNull annotation due to conflict with @NotBlank

--- src/main/java/com/example/demo/dto/LoginRequest.java
+++ src/main/java/com/example/demo/dto/LoginRequest.java
@@ -1,16 +1,13 @@
 package com.example.demo.dto;
 
 import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
 import lombok.Data;
 
 @Data
 public class LoginRequest {
     @NotBlank(message = "Email cannot be blank")
-    @NotNull(message = "Email cannot be null")
     private String email;
 
     @NotBlank(message = "Password cannot be blank")
-    @NotNull(message = "Password cannot be null")
     private String password;
 }
