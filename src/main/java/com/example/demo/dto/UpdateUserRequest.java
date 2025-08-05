85df25e57bf43b38a5cdce9254e3b57c6bd27540
Strawberry <honeykokia@gmail.com>
2025-08-05 19:52:16 +0800

    chore(UpdateUserRequest.java): - Add @NotBlank and @NotNull to validate input fields - Add userId and file field for validation not include in api-spec

--- src/main/java/com/example/demo/dto/UpdateUserRequest.java
+++ src/main/java/com/example/demo/dto/UpdateUserRequest.java
@@ -2,16 +2,46 @@ package com.example.demo.dto;
 
 import java.time.LocalDate;
 
+import org.springframework.web.multipart.MultipartFile;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.Data;
 
 @Data
 public class UpdateUserRequest {
+    @JsonIgnore
+    @Schema(hidden = true)
+    private Long userId;
+
+    @JsonIgnore
+    @Schema(hidden = true)
+    private MultipartFile file;
+
+    @NotBlank(message = "Name is required")
     private String name;
+    
+    @NotNull(message = "Gender is required")
     private Byte gender;
+
+    @NotNull(message = "birthday is required")
     private LocalDate birthday;
+
+    @NotBlank(message = "phone is required")
     private String phone;
+
+    @NotBlank(message = "image is required")
     private String image;
+
+    @NotBlank(message = "city is required")
     private String city;
+
+    @NotBlank(message = "country is required")
     private String country;
+
+    @NotBlank(message = "address is required")
     private String address;
 }
